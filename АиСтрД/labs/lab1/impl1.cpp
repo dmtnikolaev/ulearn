832bda7f58c1644044d5ac518c811ddb7a627bd5
Stepan Repin <stnrepin@gmail.com>
2019-09-18 22:42:42 +0300

    [АиСтрД.lab1]: Update impl1.cpp
    Improve the code structure and add random generation
    of input.

--- АиСтрД/labs/lab1/impl1.cpp
+++ АиСтрД/labs/lab1/impl1.cpp
@@ -1,6 +1,10 @@
+#include <stdlib.h>
+#include <time.h>
+
 #include <iostream>
 #include <string>
 #include <array>
+#include <tuple>
 
 using namespace std;
 
@@ -49,17 +53,83 @@ string ToString(array<int, MAX_LEN> arr) {
     return str;
 }
 
-int main() {
+int ReadMode() {
+    cout << "Choose program mode:" << endl;
+    cout << "1 - Random input" << endl;
+    cout << "2 - Last random" << endl;
+    cout << "3 - Read input" << endl;
+
+    int mode = -1;
+    cout << "> ";
+    cin >> mode;
+
+    return mode;
+}
+
+array<int, MAX_LEN> GenerateBitArray() {
+    static_assert(MAX_LEN <= numeric_limits<unsigned char>::digits*sizeof(int),
+        "MAX_LEN should be less than or equal to the count of bits in int.");
+
+    int random_bits = rand();
+    array<int, MAX_LEN> res;
+
+    cout << random_bits << endl;
+
+    for (auto i = 0; i < MAX_LEN; i++) {
+        res[i] = random_bits & (1 << i);
+    }
+
+    return res;
+}
+
+auto GenerateInput() {
+    auto A = GenerateBitArray();
+    auto B = GenerateBitArray();
+    auto C = GenerateBitArray();
+    auto D = GenerateBitArray();
+
+    cout << "A: " << ToString(A) << endl;
+    cout << "B: " << ToString(B) << endl;
+    cout << "C: " << ToString(C) << endl;
+    cout << "D: " << ToString(D) << endl;
+
+    return make_tuple(A, B, C, D);
+}
+
+auto ReadInputFromUser() {
     auto A = ToBitArray(ReadHex("A: "));
     auto B = ToBitArray(ReadHex("B: "));
     auto C = ToBitArray(ReadHex("C: "));
     auto D = ToBitArray(ReadHex("D: "));
 
+    return make_tuple(A, B, C, D );
+}
+
+int main() {
+    array<int, MAX_LEN> A, B, C, D;
+
+    auto mode = ReadMode();
+
+    switch (mode) {
+        case 1:
+            srand(time(nullptr));
+            [[fallthrough]];
+        case 2:
+            tie(A, B, C, D) = GenerateInput();
+            break;
+        case 3:
+            tie(A, B, C, D) = ReadInputFromUser();
+            break;
+        default:
+            cout << "Invalid mode" << endl;
+            return 0;
+    }
+
     auto res = ToString(EvaluateArray(A, B, C, D));
 
-    cout << "(A | C) & !(B | D)"
-         << endl
-         << res;
+    cout << "(A | C) & !(B | D): "
+         << res
+         << endl;
 
     return 0;
 }
