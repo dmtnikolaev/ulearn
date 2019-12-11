878104a1250da28b8c10afc665b9e52414a09fb2
Stepan Repin <stnrepin@gmail.com>
2019-12-11 19:57:13 +0300

    Create GraphData interface and implement AdjList

--- АиСтрД/labs/course/graph-data.cpp
+++ АиСтрД/labs/course/graph-data.cpp
@@ -1,1 +1,55 @@
 #include "graph-data.h"
+
+#include <stdexcept>
+#include <vector>
+
+#include "node.h"
+
+AdjList::AdjList() {
+
+}
+
+void AdjList::AddNode(Node u) {
+    if (u.id() < count()) {
+        return;
+    }
+    auto node_pos = nodes_.begin() + u.id()*sizeof(Node*);
+    nodes_.insert(node_pos, new Node(u.id()));
+    auto nei = std::vector<int>();
+    adj_list_.insert({ u.id(), nei });
+}
+
+void AdjList::AddEdge(int id1, int id2) {
+    if (id1 >= nodes_.size() ||
+        id2 >= nodes_.size()) {
+        throw std::out_of_range("id1/id2");
+    }
+
+    adj_list_[id1].push_back(id2);
+}
+
+void AdjList::AddEdge(Node u, Node v) {
+    AddNode(u);
+    AddNode(v);
+    AddEdge(v.id(), v.id());
+}
+
+Node* AdjList::GetNode(int id) const {
+    return nodes_.at(id);
+}
+
+std::vector<Node*> AdjList::GetNeigbors(int id) {
+    auto nei_ids = adj_list_[id];
+    auto nei = std::vector<Node*>();
+    for (auto nei_id : nei_ids) {
+        nei.push_back(nodes_[nei_id]);
+    }
+    return nei;
+}
+
+AdjList::~AdjList() {
+    for (auto u : nodes_) {
+        delete u;
+    }
+}
+
--- АиСтрД/labs/course/graph-data.h
+++ АиСтрД/labs/course/graph-data.h
@@ -1,8 +1,47 @@
 #ifndef COURSE_GRAPH_DATA_H_
 #define COURSE_GRAPH_DATA_H_
 
-class GraphData {
+#include <map>
+#include <vector>
 
+#include "node.h"
+
+class GraphData {
+public:
+    virtual void AddNode(Node u) = 0;
+    virtual void AddEdge(int id1, int id2) = 0;
+    virtual void AddEdge(Node u, Node v) = 0;
+
+    virtual Node* GetNode(int id) const = 0;
+    virtual std::vector<Node*> GetNeigbors(int id) = 0;
+
+    virtual int count() const = 0;
+
+    virtual ~GraphData() {
+
+    }
+};
+
+class AdjList : public GraphData {
+public:
+    AdjList();
+
+    void AddNode(Node u) override;
+    void AddEdge(int id1, int id2) override;
+    void AddEdge(Node u, Node v) override;
+
+    Node* GetNode(int id) const override;
+    std::vector<Node*> GetNeigbors(int id) override;
+
+    int count() const override {
+        return nodes_.size();
+    }
+
+    ~AdjList();
+
+private:
+    std::vector<Node*> nodes_;
+    std::map<int, std::vector<int>> adj_list_;
 };
 
 #endif  // COURSE_GRAPH_DATA_H_
