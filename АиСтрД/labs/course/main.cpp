f85d05c0cb78d8cdd288da94016414255dc30d9f
Dmitry Nikolaev <dmtnikolaev@gmail.com>
2019-12-19 13:18:18 +0300

    WIP

--- АиСтрД/labs/course/main.cpp
+++ АиСтрД/labs/course/main.cpp
@@ -6,16 +6,22 @@
 #include "graph-source.h"
 
 void PrintCycles(Graph& gr) {
+    auto cycles = gr.BuildCycles();
+    if (cycles.size() == 0) {
+        std::cout << "Graph doesn't have cycles"
+                  << std::endl;
+        return;
+    }
+
     std::cout << "Cycles: " << std::endl;
-    
-    for (auto c : gr.BuildCycles()) {
+    for (auto c : cycles) {
         std::cout << c;
     }
 }
 
-int main() {
+int main(int argc, char* argv[]) {
     try {
-        auto src = std::make_unique<TextFileSource>("input.txt");
+        auto src = std::make_unique<TextFileSource>(argv[1]);
         auto gr = Graph(src.get());
         std::cout << gr;
         PrintCycles(gr);
